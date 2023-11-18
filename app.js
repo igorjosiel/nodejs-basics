5f27ed031c58d914e1c3a78f4024c8be6e6b9a4b
Igor Josiel <igorjosielvitalino@gmail.com>
2023-11-18 14:43:53 -0300

    feat: Adicionando dois caminhos para renderizar conteúdo HTML.

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log('Req: ', req);
+const server = http.createServer((request, response) => {
+    const url = request.url;
+
+    response.setHeader('Content-Type', 'text/html');
+
+    if (url === '/') {
+        response.write('<html>');
+        response.write('<head><title>Node Js - Basics</title></head>');
+        response.write('<body><h1>Seja bem-vindo!</h1></body>');
+        response.write('</html>');
+        response.end();
+    }
+
+    if (url === '/users') {
+        response.write('<html>');
+        response.write('<head><title>Node Js - Basics</title></head>');
+        response.write(`
+            <body>
+                <h1>Usuarios:</h1>
+
+                <ul>
+                    <li>Igor</li>
+                    <li>Sofia</li>
+                    <li>Leonardo</li>
+                    <li>Franciele</li>
+                </ul>
+            </body>`
+        );
+        response.write('</html>');
+        response.end();
+    }
 });
 
 server.listen(3000);
